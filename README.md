cc7f17baccf983e0d3da7991cf22d805b04c9458
Klemen Kobau <klemen.kobau@gmail.com>
2025-07-26 09:21:47 +0200

    feat: initial reading from input stream

---
+++ README.md
@@ -0,0 +1,10 @@
+# Log nav rs
+
+Tool for parsing logs. Currently only reads from stdin
+
+## Testing
+
+Read from file continuously
+```sh
+ tail -f log.in | cargo run --
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{IsTerminal, Read, stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = [0; 1000];
+    let mut stdin = stdin();
+
+    loop {
+        let read = stdin.read(&mut buffer).unwrap();
+        if read != 0 {
+            println!("{:?}", buffer);
+        }
+    }
 }
