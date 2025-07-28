a9b6d597d4d1e46e6f46fae0e6fd961c05b42e8b
Klemen Kobau <klemen.kobau@gmail.com>
2025-07-28 20:16:45 +0200

    feat: added log4j regex

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::io::{Read, stdin};
+use std::fs::read_to_string;
+
+use fancy_regex::Regex;
 
 fn main() {
-    let mut buffer = [0; 1000];
-    let mut stdin = stdin();
+    let mut java_logs = read_to_string("log.in").unwrap();
+    let java_regex = read_to_string("java.txt").unwrap();
+
+    let re = Regex::new(&java_regex).unwrap();
 
     loop {
-        let read = stdin.read(&mut buffer).unwrap();
-        if read != 0 {
-            println!("{:?}", buffer);
-        }
+        let caps = re.captures(&java_logs).unwrap().unwrap();
+        let m = caps.get(0).unwrap();
+        println!("{:?}", m.as_str());
+        let end = m.end();
+
+        java_logs = java_logs[end..].into();
     }
 }
